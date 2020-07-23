5311ff98eb758dcbbce5f1a15a50aa600734d77b
Muhibullah-09 <mc2840696@gmail.com>
2020-07-23 20:36:29 +0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,11 +9,11 @@ function Contact() {
                 <img src='https://tilakthakurdot.files.wordpress.com/2017/11/contact-1.gif' class="rounded float-left" alt="logo"></img>
             </div>
             <div>
-                <form style={{ width: '500px', margin: '100px' }}>
-                    {/* <h1 ref={ref}><strong>Contact Us</strong></h1> */}
+                <form style={{ width: '500px', margin: '100px', padding: 0 }}>
+                    <h1 ref={ref}><strong>Contact Us</strong></h1>
                     {/* <!-- Name input --> */}
                     <div class="form-outline mb-4">
-                        <h1 ref={ref}><strong>Contact Us</strong></h1>
+                        {/* <h1 ref={ref}><strong>Contact Us</strong></h1> */}
                         <input type="text" id="form4Example1" class="form-control" />
                         <label class="form-label" for="form4Example1">Name</label>
                     </div>
