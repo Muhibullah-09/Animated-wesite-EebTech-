5c2c1603ecdbb32ec91cc7c81bfb217126cc0bb1
Muhibullah-09 <mc2840696@gmail.com>
2020-07-23 20:30:18 +0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,9 +10,10 @@ function Contact() {
             </div>
             <div>
                 <form style={{ width: '500px', margin: '100px' }}>
-                    <h1 ref={ref}><strong>Contact Us</strong></h1>
+                    {/* <h1 ref={ref}><strong>Contact Us</strong></h1> */}
                     {/* <!-- Name input --> */}
                     <div class="form-outline mb-4">
+                        <h1 ref={ref}><strong>Contact Us</strong></h1>
                         <input type="text" id="form4Example1" class="form-control" />
                         <label class="form-label" for="form4Example1">Name</label>
                     </div>
